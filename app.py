94a82e1b720ad74dae3f4bcc614e1bc8802d5c1b
youssef-omarrr <es-youssef.moh.ah2026@alexu.edu.eg>
2025-06-30 21:32:10 +0300

    test_1

--- app.py
+++ app.py
@@ -168,4 +168,5 @@ def predict():
         
         
 if __name__ == '__main__':
-    app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 for local use
+    app.run(host='0.0.0.0', port=port, debug=True)
